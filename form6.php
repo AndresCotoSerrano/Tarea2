cc8c7731470a84b0574537ccccb60394ff7520e6
AndresCotoSerrano <AndresCotoSerrano@users.noreply.github.com>
2021-06-11 13:02:10 -0600

    Update form6.php

--- form6.php
+++ form6.php
@@ -139,7 +139,7 @@
                         echo '   
                         <br>               
                         <div class="alert alert-primary" role="alert">
-                        ' . $MultiplicacionA . '
+                        ' . $FrecuenciaLinksA . '.
                       </div>';
                     }
                     ?>
