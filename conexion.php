7eb9258d5fb7975c21b95ff47a7e128641e7a68e
AndresCotoSerrano <AndresCotoSerrano@users.noreply.github.com>
2021-06-11 12:31:21 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,11 +1,11 @@
 <?php
-$link = 'mysql:host=remotemysql.com;dbname=H8ScdI0NYM';
-$usuario = 'H8ScdI0NYM';
-$pass = 'aKSFENLHE8';
+$link = 'mysql:host=remotemysql.com;dbname=XiSHtgvBeQ';
+$usuario = 'XiSHtgvBeQ';
+$pass = 'fle0ZvAld6';
 
 $con = mysqli_connect(
     'remotemysql.com',
     $usuario,
     $pass,
-    'H8ScdI0NYM'
+    'XiSHtgvBeQ'
 );
